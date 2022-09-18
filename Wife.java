cc65ebcd9b41665e479e4de4a13d7726ff97fee1
VikramDharsh <113521905+VikramDharsh@users.noreply.github.com>
2022-09-18 16:22:02 +0530

    Add files via upload

---
+++ Wife.java
@@ -0,0 +1,57 @@
+class Wife
+{
+		
+	static void wifeFunction()
+	{
+		System.out.println("she is not wife she is knife ");
+		String torcher="another name for wife";
+		int effeciency=100;
+		System.out.println("torcher percentage%---"+effeciency);
+		System.out.println("torcher---- "+torcher);
+		boolean ref=false;
+		System.out.println("my wife is beautiful with out maske up--- "+ref);
+	}
+	static void wifeQualities()
+	{
+	String height="same heightof mine";
+	System.out.println("my wife height must---"+height);
+		
+	}
+	static void love()
+	{
+	System.out.println("my should be my best friend and best enemy");
+	}
+	static void caring()
+	{
+	System.out.println("we shold be  best friends not like husband and wife");
+	
+	System.out.println("i should always 1st choice of her");
+	}
+	static void goal()
+	{
+		System.out.println("ultimate goal is 11 +2 kids");
+		
+	}
+	static void torcher()
+	{
+		System.out.println("i know she will alwaays torchers me");
+	}
+	static void lovelyWife()
+	{
+		System.out.println("she shold cook 1kg chicken");
+	}
+	static void beauty()
+	{
+		System.out.println("i dont care abt beauty ");
+	}
+	static void truth()
+	{
+		System.out.println("all i mentioned above is just for fun ");
+		System.out.println("i dont need wife i need a friend like wife-we both should fight love  ");
+		Wife.torcher();
+		goal();
+		
+		
+}
+}
+	
