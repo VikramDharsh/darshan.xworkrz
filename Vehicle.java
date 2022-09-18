72a3614e3e046379ec05dbd2f1b4e0367ecf294b
VikramDharsh <113521905+VikramDharsh@users.noreply.github.com>
2022-09-18 16:19:17 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,52 @@
+class Vehicle
+{
+	static int transportation(int distance)
+	{
+	if(distance==32)
+	{
+		System.out.println("travels from to kolar to chintamani");
+		return distance;
+	}
+	else
+	{
+		System.out.println("travels from to kolar to srinivaspura");
+return 32;
+	}
+	                                                                         // different in data type
+	}
+	
+	static byte transportation(float time)
+	{
+		if(time==8.45f)
+		{
+			System.out.println("travel  from kolar to  chinthamani");
+		return 23;
+		}
+		else
+		{
+			System.out.println("travels from kolar to  Srinivaspura");
+			return 32;
+		}
+		
+	}
+	static double transportation(int price,String name)
+	{
+		System.out.println("ticket pricr is 20 for kids");
+		System.out.println("name of bus is annapurneswari");
+		return 34;
+
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
