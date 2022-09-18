18ab77d2c48a7a25a3940bc92a0d8eb086147b62
VikramDharsh <113521905+VikramDharsh@users.noreply.github.com>
2022-09-18 16:22:52 +0530

    Add files via upload

---
+++ WifeRunner.java
@@ -0,0 +1,17 @@
+class WifeRunner
+{
+	public static void main (String[] vikram)
+	{
+		System.out.println("Time is my biggest enemy because one life is too short to love you");
+	Wife.wifeFunction();
+	Wife.wifeQualities();
+	Wife.love();
+	Wife.caring();
+	Wife.goal();
+	Wife.torcher();
+	Wife.lovelyWife();
+	Wife.beauty();
+	Wife.truth();
+	
+	}
+}
